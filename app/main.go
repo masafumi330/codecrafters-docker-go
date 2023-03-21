6d00175f74e71239fc7296c118c86c1097fe6645
Masafumi Misawa <m.masafumi@wano.co.jp>
2023-03-21 16:26:06 +0900

    :zap: pass #2

--- app/main.go
+++ app/main.go
@@ -9,19 +9,20 @@ import (
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage!
 	//
 	command := os.Args[3]
+	fmt.Printf("==========\n%#v\n", command)
 	args := os.Args[4:len(os.Args)]
+	fmt.Printf("==========\n%#v\n", args)
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
-	if err != nil {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
-
-	fmt.Println(string(output))
 }
